16e744f439ddec489056daa0cc8b682e518ba9b7
AlbertLnz <albert2000_lanza@hotmail.com>
2024-04-22 21:18:40 +0200

    POST route

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import express from "express"
+import { pool } from "./database.js"
 
 const PORT = 3000
 const app = express()
 
+// Middlewares
+app.use(express.json())
+
 app.get('/', (_req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  const { name, location } = req.body
+
+  res.status(200).send({
+    message: `YOUR KEYS WERE ${name} - ${location}`
+  })
+
 })
 
 app.listen(PORT, () => {
